65bbde96a279922ca102eb19bb10b84b99216313
Andrew Ribeiro <andrewnetwork@gmail.com>
2017-03-23 23:32:39 -0400

    dp

---
+++ HNCOMM.js
@@ -0,0 +1,9 @@
+var cls = class HNCOMM
+{
+    constructor(height, width) 
+    {
+        this.height = height;
+        this.width = width;
+    }
+}
+module.exports.HNCOMM = cls
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-var express = require('express');
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
-var app     = express();
+var hcom    = require("./HNCOMM.js");
 
-url = 'https://news.ycombinator.com/submitted?id=BucketSort';
+var newsRootURL = "https://news.ycombinator.com"
+var currLinks   = [];
+var url         = 'https://news.ycombinator.com/threads?id=BucketSort';
+var beatMS      = 50000;
+
+var a = new hcom.HNCOMM(2,3);
 
 // The structure of our request call
 // The first parameter is our URL
@@ -18,14 +22,67 @@ request(url, function(error, response, html){
         // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
 
         var $ = cheerio.load(html);
+        var links = [];
+        var linkAr = []; 
 
-        // Finally, we'll define the variables we're going to capture
+        $('.athing,.comtr').filter(function(){
+            var data    = $(this).find('.comhead');
+            var storyOn = data.find(".storyon");
+            var son     = storyOn.find('a')
 
-        var title, release, rating;
-        var json = { title : "", release : "", rating : ""};
+            if(son.html() != null){
+                var idx = newsRootURL+"/"+son.attr("href")
+                if(links[idx] == null ){
+                    links[idx]=1;
+                    linkAr.push(idx);
+                }
+            }
+            
+        })
 
-        console.log($.html())
+        currLinks = linkAr;
     }
 })
 
-exports = module.exports = app;
+function heartBeat(){
+    request(url, function(error, response, html){
+        // First we'll check to make sure no errors occurred when making the request
+
+        if(!error){
+                // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+
+                var $      = cheerio.load(html);
+                var links  = [];
+                var linkAr = []; 
+
+                $('.athing,.comtr').filter(function(){
+                    var data    = $(this).find('.comhead');
+                    var storyOn = data.find(".storyon");
+                    var son     = storyOn.find('a')
+
+                    if(son.html() != null){
+                        var idx = newsRootURL+"/"+son.attr("href")
+                        if(links[idx] == null ){
+                            links[idx]=1;
+                            linkAr.push(idx)
+                        }
+                        
+                    }
+                    
+                })
+            
+                console.log(currLinks[0] + "   "+ linkAr[0])
+                if(currLinks[0] != linkAr[0]){
+                    console.log("New link: "+linkAr[0]);
+                    
+                    currLinks = linkAr;
+                }
+                else
+                {
+                    console.log("No new links.");
+                }
+            }
+        })
+    }
+
+setInterval(heartBeat,beatMS);
